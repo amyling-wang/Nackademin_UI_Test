701cd3914055db638bca1e01a34c05c691ebd346
Ling Wang <amyling.wang@gmail.com>
2024-01-10 00:21:42 +0100

    reset and fix bugs

--- FacebookTest/Config/ConfigValues.cs
+++ FacebookTest/Config/ConfigValues.cs
@@ -6,11 +6,14 @@ namespace FacebookTest.Config
     {
         private static IConfigurationRoot _configuration = new ConfigurationBuilder().Build();
         private static string _url;
+        private static string _username;
+        private static string _password;
         public static void LoadConfiguration()
         {
             //var enviromentName = Environment.GetEnvironmentVariable("ENVIRONMENT");
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("secrets.json")
                 .AddJsonFile("appsettings.json");
                // .AddJsonFile($"appsettings.{enviromentName}.json", true);
             _configuration = builder.Build();            
@@ -27,6 +30,28 @@ namespace FacebookTest.Config
                 return _url; 
             } 
             set { _url = value; }
+        }
+        public static string Username
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_username))
+                {
+                    _username = _configuration.GetSection("USERNAME").Value;
+                }
+                return _username;
+            }
+        }
+        public static string Password
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_password))
+                {
+                    _password = _configuration.GetSection("PASSWORD").Value;
+                }
+                return _password;
+            }
         }
     }
 }
