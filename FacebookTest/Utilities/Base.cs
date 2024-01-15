23150383ae88c65cfb712e5ca1029b3d90d0a9d4
Ling Wang <amyling.wang@gmail.com>
2024-01-15 10:18:47 +0100

    fix

---
+++ FacebookTest/Utilities/Base.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+
+namespace FacebookTest.Utilities
+{
+    public class Base
+    {
+        //protected IWebDriver driver;
+        //public Base(IWebDriver driver) {  this.driver = driver; }
+
+        //public static IWebElement FindElement(By by)
+        //{
+        //    return driver.FindElement(by);
+        //}
+
+    }
+}
---
+++ FacebookTest/Utilities/WaitHelper.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookTest.Utilities
+{
+    public static class WaitHelper
+    {
+        //static readonly IWebDriver driver = 
+
+
+        //public static void WaitAndClick(this IWebElement locator)
+        //{
+        //    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        //    IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+        //    element.Click();
+        //}
+    }
+}
