b75abe0993f2986fd7dd7fe20edf8ac0e88ebf1e
Ling Wang <ling.wang@snowsoftware.com>
2024-05-17 11:15:08 +0200

    Add config.yml

---
+++ NackademinUITest/Features/Home.feature
@@ -0,0 +1,91 @@
+﻿Feature: Home
+
+Background: 
+	Given I navigate to Nackademin
+	
+@Smoke
+Scenario: Verify main image section on Home
+	Then I should see image with text 'Din framtid börjar här'
+	When I click on the button Hitta utbildning
+	Then I should be navigated to Utbildningar
+
+Scenario: Verify Program, Kurser and För Företag section
+	Then I verify below mentiond cards and related links for them on Home page
+		| Cart name   |
+		| Program     |
+		| Kurser      |
+		| För Företag |
+
+Scenario: Verify sections with with link to Om oss, Antagning and Hitta drömjobbet
+	Then I Verify below sections on the page
+		| Section title                     | Section category      | Section link     |
+		| 30 år nära arbetslivet            | NACKADEMIN            | Om oss           |
+		| Starta din framtid hos Nackademin | ANTAGNING             | Antagning        |
+		| Nå ditt drömjobb med Nackademin   | VÄGEN TILL DRÖMJOBBET | Hitta drömjobbet |
+
+Scenario: Verify Inspiration section on Home page
+	Then I should see 3 articles by default under section with title Inspiration
+	When I click on the button Visa fler artiklar
+	Then I should be navigated to Inspiration
+	
+Scenario: Verify section for Frågor och Svar
+	Then I should see section with title Frågor och svar
+	And I should see information text for section Frågor och svar
+	When I click on any question
+	Then I should see related content under it
+
+Scenario Outline: Verify news letter section
+	Then I should see news letter section title on the page
+	Then I enter <Email> in Din e-postadress field in section Få vårt nyhetsbrev
+	When I click on button Prenumerera in section for Få vårt nyhetsbrev
+	Then I should see a message contains <Message> in the section
+
+Examples: 
+	| Email                   | Message |
+	| name.eftername.test.com | problem |
+	| name.eftername@test.com | Tack    |
+
+Scenario: Verify sidfooter for Home page
+	When I click on link navigates to Home page in page footer
+	Then I verify below mentioned links in site footer section
+		| Link name       | Landing page  |
+		| Utbildningar    | Utbildningar  |
+		| Antagning       | Antagning     |
+		| För företag     | För företag   |
+		| Inspiration     | Inspiration   |
+		| Om Nackademin   | Om Nackademin |
+		| Pressrum        | Pressrum      |
+		| Kontakta oss    | Kontakta oss  |
+		| Medarbetare     | Medarbetare   |
+		| Frågor och svar | FAQ           |
+
+Scenario: Verify post email in sidfooter section on Home page
+	Then I enter <Email> in Din e-postadress field in section site footer
+	When I click on button Prenumerera in section for site footer
+	Then I should see a message contains <Message> in the section
+
+Examples: 
+	| Email                   | Message |
+	| name.eftername.test.com | problem |
+	| name.eftername@test.com | Tack    |
+
+Scenario: Verify Home Page meny section
+	Then I click on button beside Utbildningar in meny section
+	Then I verify below mentioned links in meny section under Utbildningar
+		| Link name         | Landing page      |
+		| Alla utbildningar | Utbildningar      |
+		| Kurser            | Kurs              |
+		| Program           | Program           |
+		| Platsbunden       | Platsbunden       |
+		| Distans           | Distans           |
+		| Online            | Online            |
+		| YH-flex           | YH-flex           |
+		| Design            | Design            |
+		| Elteknik & energi | Elteknik & energi |
+		| IT                | IT                |
+		| Kommunikation     | Kommunikation     |
+		| Samhällsbyggnad   | Samhällsbyggnad   |
+		| Spel              | Spel              |
+		| Vård              | Vård & hälsa      |
+
+		
---
+++ NackademinUITest/Utilities/SeleniumExtensions.cs
@@ -0,0 +1,333 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using FacebookTest.Config;
+using OpenQA.Selenium.Interactions;
+using System.Diagnostics;
+
+namespace FacebookTest.Utilities
+{
+    public static class SeleniumExtensions
+    {        
+        private static IWebDriver GetDriver()
+        {
+            return DriverManager.GetDriver();
+        }       
+        public static IWebElement FindElement(this By locator)
+        {
+            locator.WaitToBecomeAvailable();
+            return GetDriver().FindElement(locator);
+        }
+        private static WebDriverWait CreateWait()
+        {
+            ConfigValues config = new();
+            return new WebDriverWait(GetDriver(), TimeSpan.FromMilliseconds(config.PageLoadTiemout()));
+        }
+        public static void WaitAndClick(IWebElement element)
+        {
+            WebDriverWait wait = new WebDriverWait(GetDriver(), TimeSpan.FromSeconds(10));            
+            IWebElement webelement = wait.Until(ExpectedConditions.ElementToBeClickable(element));
+            webelement.Click();
+        }
+        
+        public static void WaitUntilElementIsVisible(this By locator, double? timeToWaitInMilliSeconds = null, string locatorDetails = null)
+        {
+            try
+            {
+                var wait = CreateSpecificTimeWait(timeToWaitInMilliSeconds);
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+            }
+            catch (Exception e)
+            {
+                throw new WebDriverTimeoutException($"Locator for {locatorDetails} is not visible on the webpage, exception message {e.Message}");
+            }
+        }
+
+        public static void WaitToBecomeAvailable(this By locator, string locatorName = null, double? timeToWaitInMilliSeconds = null)
+        {
+            try
+            {
+                var wait = CreateSpecificTimeWait(timeToWaitInMilliSeconds);
+                wait.Until(ExpectedConditions.ElementExists(locator));
+            }
+            catch (Exception e)
+            {
+                if (!string.IsNullOrEmpty(locatorName))
+                {
+                    throw new WebDriverTimeoutException($"Locator '{locatorName}' is not displayed on the page, Exception : {e.Message}");
+                }
+                else
+                {
+                    throw new WebDriverTimeoutException($"Locator is not displayed on the page, Exception : {e.Message}");
+                }
+            }
+        }
+    
+        public static void WaitToBecomeClickable(this By locator, string locatorName = null)
+        {
+            locator.WaitToBecomeEnabled();
+            try
+            {
+                var wait = CreateWait();
+                wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+            }
+            catch (Exception e)
+            {
+                if (!string.IsNullOrEmpty(locatorName))
+                {
+                    throw new WebDriverTimeoutException($"Locator '{locatorName}' is still not clickable, exception message " + e.Message);
+                }
+                else
+                {
+                    throw new WebDriverTimeoutException($"Locator is still not clickable, exception message " + e.Message);
+                }
+            }
+        }
+        public static void WaitToBecomeEnabled(this By locator)
+        {
+            ConfigValues config = new();
+            int secondsToWait = config.PageLoadTiemout() / 1000;
+            for (int i = 0; i < secondsToWait; i++)
+            {
+                if (locator.IsExist())
+                {
+                    break;
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                if (i == secondsToWait - 1)
+                {
+                    throw new ApplicationException($"Element is not enabled after waiting for " + secondsToWait + " seconds");
+                }
+            }
+        }
+        public static bool IsExist(this By locator)
+        {
+            return GetDriver().FindElements(locator).Any();
+        }
+
+        public static bool IsDisplayed(this By locator)
+        {
+            return locator.FindElement().Displayed;
+        }
+        private static WebDriverWait CreateSpecificTimeWait(double? timeToWaitInMilliSeconds = null)
+        {
+            if (timeToWaitInMilliSeconds == null)
+            {
+                ConfigValues config = new();
+                timeToWaitInMilliSeconds = config.PageLoadTiemout();
+            }
+            return new WebDriverWait(GetDriver(), TimeSpan.FromMilliseconds((double)timeToWaitInMilliSeconds));
+        }
+        public static void WaitAndClickElement(this By locator, string locatorDetails = null)
+        {
+            locator.WaitUntilElementIsVisible(locatorDetails : locatorDetails);
+            locator.WaitToBecomeClickable(locatorDetails);
+
+            ClickElement(locator, locatorDetails);
+        }
+        public static void ClickElement(this By locator, string locatorDetails = null)
+        {
+            locator.WaitToBecomeAvailable(locatorDetails);
+            var jsDriver = (IJavaScriptExecutor)GetDriver();
+            var element = locator.FindElement();
+            string highlightJavascript = @"arguments[0].style.cssText = ""border-width: 2px; border-style: solid; border-color: green"";";
+            jsDriver.ExecuteScript(highlightJavascript, new object[] { element });
+
+            try
+            {
+                locator.FindElement().Click();
+            }
+            catch (Exception e)
+            {
+                if (locatorDetails is not null)
+                {
+                    throw new ApplicationException($"Exception occured while clicking on element, {locatorDetails} " + e.Message);
+                }
+                else
+                {
+                    throw new ApplicationException($"Exception occured while clicking on element, " + e.Message);
+                }
+            }
+        }
+        public static void MoveToElementAndClick(this By locator, string locatorDetails = null)
+        {
+            locator.MoveToElement();
+            locator.WaitAndClickElement(locatorDetails);
+        }
+        public static void MoveToElement(this By locator, string locatorDetails = null, bool isScrollIntoView = true)
+        {
+            locator.WaitToBecomeAvailable();
+            string scriptToExecute = @"arguments[0].scrollIntoView({block:'center'});";
+
+            try
+            {
+                if (isScrollIntoView)
+                {
+                    ((IJavaScriptExecutor)GetDriver()).ExecuteScript(scriptToExecute, locator.FindElement());
+                }
+            }
+            catch (Exception)
+            {
+                if (locatorDetails is not null)
+                {
+                    throw new ApplicationException($"Exception occured while moving to an element '{locatorDetails}' using javascript");
+                }
+                else
+                {
+                    throw new ApplicationException($"Exception occured while moving to an element using javascript");
+                }
+            }
+
+
+            Actions actions = new(GetDriver());
+            actions.MoveToElement(locator.FindElement());
+
+            actions.Perform();
+        }
+
+        public static void ClickUsingJavaScriptExecutor(this By locator, string locatorDetails = null)
+        {
+            locator.WaitToBecomeAvailable(locatorDetails);
+            var jsDriver = (IJavaScriptExecutor)GetDriver(); 
+            var element = locator.FindElement();
+            string highlightJavascript = @"arguments[0].style.cssText = ""border-width: 2px; border-style: solid; border-color: red"";";
+            jsDriver.ExecuteScript(highlightJavascript, new object[] { element });
+            try
+            {
+                ((IJavaScriptExecutor)GetDriver()).ExecuteScript("arguments[0].click();", element);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Exception occured while clicking {locatorDetails} using javascript, exception message " + ex.Message);
+            }
+
+        }
+        public static void SendKeys(this By locator, string text)
+        {
+            locator.WaitToBecomeAvailable();
+            locator.FindElement().SendKeys(text);
+        }
+        public static void Clear(this By locator)
+        {
+            locator.WaitToBecomeAvailable();
+            locator.WaitUntilElementIsVisible();
+            locator.WaitToBecomeEnabled();
+            locator.FindElement().Clear();            
+        }
+        public static void ClearThenSendKeysAndWait(this By locator, string text)
+        {
+            locator.Clear();
+            locator.SendKeys(text);
+        }
+        public static string GetAttributeValue(this By locator, string attributeName)
+        {
+            locator.WaitToBecomeAvailable();
+            string attributeValue;
+            try
+            {
+                attributeValue = locator.FindElement().GetAttribute(attributeName);
+            }
+            catch (StaleElementReferenceException)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                attributeValue = locator.FindElement().GetAttribute(attributeName);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Unable to get attribute value for attribute '" + attributeName + "', exception occured " + ex.Message);
+            }
+            return attributeValue;
+        }
+        public static string GetText(this By locator)
+        {
+            return locator.FindElement().Text.Trim();
+        }
+        public static string GetText(this IWebElement webElement)
+        {
+            int counter = 0;
+            int retryCounterForWorkaround = 25;
+            while (counter < retryCounterForWorkaround)
+            {
+                try
+                {
+                    return webElement.Text;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    if (++counter == retryCounterForWorkaround)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return webElement.Text;
+        }
+        public static int GetCountOfElements(this By locator)
+        {
+            return GetDriver().FindElements(locator).Count;
+        }
+        public static void SendKeysAndWait(this By locator, string text)
+        {
+            locator.WaitToBecomeAvailable();
+            locator.WaitToBecomeEnabled();
+            locator.WaitToBecomeClickable();
+            locator.FindElement().SendKeys(text);
+        }
+        public static void WaitForPresenceOfAllElements(this By locator, double? timeToWaitInMilliSeconds = null)
+        {
+            try
+            {
+                var wait = CreateSpecificTimeWait(timeToWaitInMilliSeconds);
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.PresenceOfAllElementsLocatedBy(locator));
+            }
+            catch (Exception ex)
+            {
+                throw new WebDriverTimeoutException($"Elements are not still present on the page {ex.Message}");
+            }
+        }
+        public static bool IsStale(this IWebElement element)
+        {
+            return ExpectedConditions.StalenessOf(element)(GetDriver());
+        }
+        public static List<string> GetTextFromListOfElements(this By locator)
+        {
+            var listOfText = new List<string>();
+            var elements = GetDriver().FindElements(locator);
+            if (elements.Count > 0)
+            {
+                locator.WaitForPresenceOfAllElements();
+                bool isStale = true;
+                Stopwatch sw = new();
+                sw.Start();
+                ConfigValues config = new();
+                while (isStale)
+                {
+                    for (int i = 0; i < elements.Count; i++)
+                    {
+                        if (elements[i].IsStale())
+                        {
+                            break;
+                        }
+                        if (i == elements.Count - 1)
+                        {
+                            isStale = false;
+                        }
+                    }
+                    elements = GetDriver().FindElements(locator);
+                    if (sw.ElapsedMilliseconds > config.PageLoadTiemout())
+                    {
+                        throw new ApplicationException($"Could not complete the operation in the provided time of {config.PageLoadTiemout} milliseconds");
+                    }
+                }
+                sw.Stop();
+                foreach (var element in elements)
+                {
+                    listOfText.Add(element.GetText().Trim());
+                }
+            }
+
+            return listOfText;
+        }
+    }
+}
